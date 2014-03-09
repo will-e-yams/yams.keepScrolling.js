29331a51ad78b6d3321c41845912cf20e7215890
will-e-yams <brad.lee.williams@gmail.com>
2014-03-09 11:13:29 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Keep Scrolling
+
+infinite scrolling
+
+- 
+
+### Requires
+
+- [jQuery](#http://jQuery.com)
+
+### Usage
+
+_*Note:* requires [jQuery](http://jQuery.com)_
+
+```
+<script type="text/javascript">
+	$(function() {
+		$('.keep-scrolling').keepScrolling();
+	})
+</script>
+
+```
---
+++ yams.keepScrolling.js
@@ -0,0 +1,111 @@
+﻿/* ========================================================================
+* yams.keepScrolling.js
+* version:	1.0
+* author:	Brad Williams <brad.lee.williams@gmail.com>
+*           thanks Bootstrap for the plugin template
+* ======================================================================== */
+
++function ($) {
+    'use strict';
+
+    // keepScrolling PUBLIC CLASS DEFINITION
+    // ===============================
+
+    var KeepScrolling = function (element, options) {
+        this.options =
+		this.element = null
+
+        this.init('keepScrolling', element, options)
+    }
+
+    KeepScrolling.DEFAULTS = {
+        pagenumber: 1,
+        pagesize: 10,
+        auto: false,
+        loadMoreTemplate: '<div class="clearfix keepScrolling more">Load More</div>',
+    }
+
+    KeepScrolling.prototype.init = function (type, element, options) {
+        this.type = type
+        this.$element = $(element)
+        this.options = this.getOptions(options)
+
+        if (!this.options.url)
+            alert('keepScrolling.url is undefined');
+
+        // load initial data
+        this.loadMore(this.options.pagenumber);
+
+        // load more button
+        this.$element.on('click.yams.keepScrolling', '.keepScrolling.more', $.proxy(function () {
+            this.loadMore(parseInt(this.lastpageloaded) + 1);
+        }, this));
+
+        /* monitor scroll event
+        this.$element.on('scroll.yams.keepScrolling', $.proxy(function () {
+            console.log('scroll');
+            console.log(this.$element.scrollTop);
+        }, this));*/
+    }
+
+    KeepScrolling.prototype.getDefaults = function () {
+        return KeepScrolling.DEFAULTS
+    }
+
+    KeepScrolling.prototype.getOptions = function (options) {
+        options = $.extend({}, this.getDefaults(), this.$element.data(), options)
+        return options
+    }
+
+    KeepScrolling.prototype.scroll = function () {
+        console.log('scroll');
+    }
+
+    KeepScrolling.prototype.loadMore = function (pagenumber) {
+        var qsChar = '?';
+        if (this.options.url.indexOf('?') >= 0)
+            qsChar = '&';
+
+        this.lastpageloaded = pagenumber;
+        this.nextpage = $('<div class="keepScrolling" data-keepScrolling-page="' + pagenumber + '"></div>');
+        this.nextpage.load(this.options.url + qsChar + 'pagesize=' + this.options.pagesize + '&pagenumber=' + pagenumber,
+               $.proxy(function (e) {
+                   this.$element.find('.keepScrolling.more').remove();
+                   this.$element.trigger('loaded.yams.keepScrolling');
+                   this.$element.append(this.nextpage);
+                   // add button
+                   if (!this.options.auto)
+                       this.$element.append(this.options.loadMoreTemplate);
+               }, this)
+        );
+    }
+
+    // keepScrolling PLUGIN DEFINITION
+    // =========================
+
+    var old = $.fn.keepScrolling
+
+    $.fn.keepScrolling = function (option) {
+        return this.each(function () {
+            var $this = $(this)
+            var data = $this.data('yams.keepScrolling')
+            var options = typeof option == 'object' && option
+
+            if (!data) $this.data('yams.keepScrolling', (data = new KeepScrolling(this, options)))
+            if (typeof option == 'string') data[option]()
+        })
+    }
+
+    $.fn.keepScrolling.Constructor = KeepScrolling
+
+
+    // keepScrolling NO CONFLICT
+    // ===================
+
+    $.fn.keepScrolling.noConflict = function () {
+        $.fn.keepScrolling = old
+        return this
+    }
+
+}(jQuery);
+
